4d8b4ff4eda8e439b0217f31707f3cf96fcca9b0
itz <75608529+itz0@users.noreply.github.com>
2022-07-17 16:31:38 +0300

    Update youtube.bat

--- youtube.bat
+++ youtube.bat
@@ -3,11 +3,15 @@ rem del video.mkv
 set VLC="C:\Program Files\VideoLAN\VLC\vlc.exe"
 set MPC="C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe"
 set /p yturl=Please insert full Youtube video link (right-click): 
-youtube-dl -f bestvideo+bestaudio/best %yturl:&=^&% -o "video"
+yt-dlp -f bestvideo+bestaudio/best %yturl:&=^&% -o "video"
+
+rem for using socks proxy edit & use this run line:
+rem yt-dlp --proxy socks5://user:pass@127.0.0.1:1080/ -f bestvideo+bestaudio/best %yturl:&=^&% -o "video"
+
 move video.mp4 video.mkv
 cls
 echo Downloaded file video.mkv from youtube, press any key and check dir
-rem %VLC% --meta-title=youtube-dl video.mkv
+rem %VLC% --meta-title=yt-dlp video.mkv
 rem %MPC% video.mkv
 rem del video.mkv
 pause
